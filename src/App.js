fa1f8d0a293b6fa99c946fcf5b43d6fab7c62bb6
s u m a n t h <badampraveena1981@gmail.com>
2023-08-09 13:14:00 +0530

    nothind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/Textform';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 
  
@@ -35,22 +41,23 @@ function App() {
   }
   return (
     <>
+    <Router>
     
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
     <Alert alert={alert}/>
     <div className="container my-3">
-    {/* <Switch> */}
+    <Switch> 
     {/* /users --> Component 1
         /users/home --> Component 2 */}
-          {/* <Route exact path="/about">
+           <Route exact path="/about">
             <About mode={mode} />
-          </Route> */}
-          {/* <Route exact path="/"> */}
+          </Route>
+            <Route exact path="/"> 
             <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}></TextForm>
-          {/* </Route> */}
-    {/* </Switch> */}
+          </Route> 
+    </Switch>
     </div>
-    
+    </Router>
     </> 
   );
 }
