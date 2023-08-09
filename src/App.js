28210c2994a9ab48f02a040df48136b8715ca473
s u m a n t h <badampraveena1981@gmail.com>
2023-08-09 12:50:40 +0530

    chnagess

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import './App.css';
-import About from './components/About';
 import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+import TextForm from './components/Textform';
+// import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 
+
+ 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
     <>
-    <Navbar title="MY-APP" />
+    
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <Alert alert={alert}/>
     <div className="container my-3">
-      <h1>Enter the text to analyze</h1>
-    <Textform  heading="Enter your story"/>
+    {/* <Switch> */}
+    {/* /users --> Component 1
+        /users/home --> Component 2 */}
+          {/* <Route exact path="/about">
+            <About mode={mode} />
+          </Route> */}
+          {/* <Route exact path="/"> */}
+            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}></TextForm>
+          {/* </Route> */}
+    {/* </Switch> */}
     </div>
-    <div className="container my-3">
-    <About />
-    </div>
-    </>  
+    
+    </> 
   );
 }
 
